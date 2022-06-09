8d5b92ef7e5ba93f5fe9979f16488ef8a1873be3
MMSabbaghi <mmsabbaghi1399@gmail.com>
2022-06-09 13:28:00 +0430

    first commit

---
+++ app.js
@@ -0,0 +1,95 @@
+const cheerio = require("cheerio");
+const axios = require("axios");
+const JobSearchSites = require("./jobsearchsites");
+
+async function loadData(url) {
+  try {
+    const { data } = await axios.get(url);
+    return cheerio.load(data);
+  } catch (error) {
+    console.log(error);
+    throw new Error(error.code || "Error");
+  }
+}
+
+async function getJobs({ url, siteName, selectors }) {
+  const $ = await loadData(url);
+
+  const jobs = $(selectors.card).map((_i, el) => {
+    const findEl = (selector) => $(el).find(selector);
+    const selectInfo = (selector) => findEl(selector).text().trim();
+    const selectLink = ($, attr) => selectors.root + findEl($).attr(attr);
+
+    return {
+      title: selectInfo(selectors.title),
+      place: selectInfo(selectors.place),
+      company: selectInfo(selectors.company),
+      link: selectLink(selectors.link, "href"),
+      img: selectLink(selectors.img, "src"),
+      siteName,
+    };
+  });
+
+  return jobs.toArray();
+}
+
+async function getAllJobs() {
+  const jobPromises = JobSearchSites.map((site) => getJobs(site));
+  const jobs = await Promise.all(jobPromises);
+  return jobs.flat();
+}
+
+function renderJob(jobInfo) {
+  const { title, place, img, company, siteName, link } = jobInfo;
+  return `
+  <div class="card mb-3">
+  <div class="row g-0">
+  <div class="col-sm-1">
+  <img src="${img}" class="w-100 rounded-start">
+  </div>
+  <div class="col-sm-11">
+  <div class="card-body">
+      <a href="${link}" style="text-decoration: none;" >
+        <h5 class="card-title">${title || "-"}</h5>
+      </a>
+        <ul class="list-group list-group-flush bg-light">
+          <li class="list-group-item">${company || "-"}</li>
+          <li class="list-group-item">${place || "-"}</li>
+          <li class="list-group-item text-success fw-bold">« ${
+            siteName || "-"
+          } »</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+  `;
+}
+
+function shuffle(array) {
+  return array.sort(() => Math.random() - 0.5);
+}
+
+function renderJobs(jobs) {
+  const jobWrapper = document.querySelector(".job-cards");
+  jobWrapper.innerHTML = "";
+  shuffle(jobs).forEach((job) => (jobWrapper.innerHTML += renderJob(job)));
+}
+
+function renderError() {
+  const fetchStatusEl = document.querySelector(".job-fetch-status");
+  fetchStatusEl.parentElement.classList.replace("bg-dark", "bg-danger");
+  fetchStatusEl.innerHTML = `
+  <h5 class="text-light mb-3">  خطا در دریافت اطلاعات </h5>
+  <button class="btn btn-light btn-sm" onclick="window.location.reload()"> بارگذاری مجدد !</button>
+  `;
+}
+
+(async function main() {
+  try {
+    const jobs = await getAllJobs();
+    renderJobs(jobs);
+  } catch (error) {
+    renderError();
+  }
+})();
---
+++ jobsearchsites.js
@@ -0,0 +1,30 @@
+module.exports = [
+  {
+    id: "e-estekhdam",
+    url: "https://www.e-estekhdam.com/search/%D8%A7%D8%B3%D8%AA%D8%AE%D8%AF%D8%A7%D9%85-%D8%A8%D8%B1%D9%86%D8%A7%D9%85%D9%87-%D9%86%D9%88%DB%8C%D8%B3",
+    siteName: "ای استخدام",
+    selectors: {
+      root: "https://www.e-estekhdam.com",
+      card: ".job-list-item",
+      title: "a.title",
+      link: "a.title",
+      company: ".company > a",
+      place: ".provinces > span",
+      img: `a.logo > img`,
+    },
+  },
+  {
+    id: "jobinja",
+    url: "https://jobinja.ir/jobs/category/it-software-web-development-jobs/%D8%A7%D8%B3%D8%AA%D8%AE%D8%AF%D8%A7%D9%85-%D9%88%D8%A8-%D8%A8%D8%B1%D9%86%D8%A7%D9%85%D9%87-%D9%86%D9%88%DB%8C%D8%B3-%D9%86%D8%B1%D9%85-%D8%A7%D9%81%D8%B2%D8%A7%D8%B1?preferred_before=1654611851&sort_by=relevance_desc",
+    siteName: "جابینجا",
+    selectors: {
+      root: "",
+      card: ".o-listView__itemInfo",
+      title: "a.c-jobListView__titleLink",
+      link: "a.c-jobListView__titleLink",
+      company: ".c-icon--construction + span",
+      place: ".c-icon--place + span",
+      img: ".o-listView__itemIndicatorImage",
+    },
+  },
+];
